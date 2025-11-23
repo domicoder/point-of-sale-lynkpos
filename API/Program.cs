8568e2668ce7f047ed04eb5326266071a242947e
Yander Sanchez <sanchezyander@gmail.com>
2025-11-22 21:49:55 -0400

    chore: add automatic migrations

--- API/Program.cs
+++ API/Program.cs
@@ -103,6 +103,12 @@ if (storagePublicPath != null)
     });
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
