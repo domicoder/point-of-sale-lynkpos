db2284fe8ee7f0afbb59d5991c538a0077c1668f
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-12-02 18:57:15 -0400

    feat: Initialize properties to null in CajaControllerEditOneDto

--- Domain/Controller/Private/Caja/CajaControllerEditOneDto.cs
+++ Domain/Controller/Private/Caja/CajaControllerEditOneDto.cs
@@ -3,8 +3,8 @@ namespace Domain.Controller.Private.Caja
 {
     public class CajaControllerEditOneDto
     {
-        public string? Codigo { get; set; }
-        public string? Nombre { get; set; }
-        public bool? Activo { get; set; }
+        public string? Codigo { get; set; } = null;
+        public string? Nombre { get; set; } = null;
+        public bool? Activo { get; set; } = null;
     }
 }
