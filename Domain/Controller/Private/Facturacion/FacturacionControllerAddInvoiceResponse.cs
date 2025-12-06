0cc6fe4ec5468bbfab79d5d1f393a4d5bcff87b1
Máximo Liberata Torres <maximoliberata0@gmail.com>
2025-12-05 20:35:11 -0400

    feat: Update invoice creation response to include invoice ID and add response model

---
+++ Domain/Controller/Private/Facturacion/FacturacionControllerAddInvoiceResponse.cs
@@ -0,0 +1,11 @@
+﻿using Domain.API;
+
+namespace Domain.Controller.Private.Facturacion
+{
+    public class FacturacionControllerAddInvoiceResponse : BaseObjectResponse<Data> {}
+
+    public class Data
+    {
+        public Guid Id { get; set; }
+    }
+}
